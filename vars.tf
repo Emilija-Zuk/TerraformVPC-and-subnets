f79de80611ac8f5b74f60dd4e07f952e724cf0f3
emilijaz <emilija.zuk@powertec.com.au>
2024-06-26 11:16:19 +1000

    initial commit

---
+++ vars.tf
@@ -0,0 +1,39 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "ZONE2" {
+  default = "us-east-1b"
+}
+
+variable "ZONE3" {
+  default = "us-east-1c"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-04e8b3e527208c8cf"
+    us-east-2 = "ami-0db513e73ed5a7faa"
+  }
+}
+
+variable "USER" {
+  default = "ec2-user"
+}
+
+variable "PUB_KEY" {
+  default = "dovekey.pub"
+}
+
+variable "PRIV_KEY" {
+  default = "dovekey"
+}
+
+variable "MYIP" {
+  default = "111.111.111.111"
+}
